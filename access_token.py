c403edae816d381fdc788b1d14cf40fd27a1d58e
coomaso <74811237+coomaso@users.noreply.github.com>
2025-03-21 13:13:20 +0800

    Update access_token.py

--- access_token.py
+++ access_token.py
@@ -19,8 +19,7 @@ attempt = 0  # 计数器
 
 BASE_url = "https://zhcjsmz.sc.yichang.gov.cn"
 # 获取脚本当前目录
-script_dir = Path(__file__).parent
-TOKEN_file = script_dir / "access_token.json"
+TOKEN_file = Path("/tmp/access_token.json")
 
 headers = {
  "Host": "zhcjsmz.sc.yichang.gov.cn",
